24a0384cd06b159062a43c5b545f19e342761e82
Nicole Hu <nicoleyghu@gmail.com>
2022-08-21 16:54:55 -0400

    Add docs.

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = "AmpTorch"
+copyright = "2022, Ulissi Group"
+author = "Ulissi Group"
+
+release = "0.1"
+version = "0.1.0"
+
+# -- General configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+exclude_patterns = []
+
+# -- Options for HTML output
+
+html_theme = "furo"
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
