d589547593bf540cca35a40a646c9c64b4e8d4f9
Nicole Hu <nicoleyghu@gmail.com>
2022-08-21 17:24:34 -0400

    Fix index for docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,16 +13,24 @@ version = "0.1.0"
 
 extensions = [
     "myst_parser",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
 
 exclude_patterns = []
+
+master_doc = "index"
 
 # -- Options for HTML output
 
