4ca435da48a63b4164b3513ff5876178c3b7fd19
Nicole Hu <nicoleyghu@gmail.com>
2022-08-21 20:51:22 -0400

    Update conf.py file.

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,13 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# The suffix of source filenames.
+source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
+
+spelling_lang = "en_US"
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -47,6 +54,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_theme_path = [
+    "_themes",
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
